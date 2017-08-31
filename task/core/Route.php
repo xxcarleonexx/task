b036b271c9f98f3b6c48ee6f9ee6f0c8bfbc992b
carleone <rusanovserg@gmail.com>
2017-08-31 22:44:48 +0300

    Continue working.

--- task/core/Route.php
+++ task/core/Route.php
@@ -8,12 +8,25 @@ class Route
 {
 
     private $server;
+    private $fullPath;
+    private $requestedPath;
+    private $post;
+    private $get;
     private $queryPath;
 
-    public function __construct($server, $query)
+    public function __construct()
     {
-        $this->server = $server;
-        $this->queryPath = $query;
+        $this->queryPath = $_SERVER['REQUEST_URI'];
+        $this->requestedPath = $_SERVER['QUERY_STRING'];
+        $this->fullPath = $_SERVER['SERVER_NAME'] . $this->queryPath;
+        $this->server = $_SERVER;
+        $this->get = $_GET;
+        $this->post = $_POST;
+    }
+
+    public function getPath()
+    {
+        return parse_url($this->fullPath);
     }
 
 }
